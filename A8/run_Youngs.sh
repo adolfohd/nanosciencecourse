7eee94ff462241ccb9f8c130ebf4079f513f258a
Adolfo Hoyos <adolfo.hoyos@correounivalle.edu.co>
2016-11-01 10:28:42 -0500

    added comments for Young module script. Added copy of this for starting a script for Bulk's module

--- A8/run_Youngs.sh
+++ A8/run_Youngs.sh
@@ -5,9 +5,9 @@ echo $2
 # lmp_serial.exe -in in.Al2
 lammps < $1 > $2
 
-grep "Length, Energy " log.lammps > l
+grep "length, pressure =" log.lammps > l
 grep -v "print" l > l1
 sed -e 's/Length, Energy (eV and kcal\/mol), pressure = //g' l1 > l2
 sort -k1n l2 > eos
 rm l l1 l2
-python murnaghan.py eos
+# python murnaghan.py eos
