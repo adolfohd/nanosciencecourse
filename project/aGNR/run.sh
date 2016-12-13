6c5b11046ea7dcf91f5ddb1c15c22ea208052e31
Adolfo Hoyos <adolfo.hoyos@correounivalle.edu.co>
2016-12-13 00:07:12 -0500

    more and more cluster

--- project/aGNR/run.sh
+++ project/aGNR/run.sh
@@ -2,8 +2,8 @@
 #SBATCH -n 8                    # Number of cores
 #SBATCH -N 1                    # Ensure that all cores are on one machine
 #SBATCH -t 10-00:05              # Runtime in D-HH:MM
-#SBATCH -p serial_requeue       # Partition to submit to
-#SBATCH --mem=100               # Memory pool for all cores (see also --mem-per-cpu)
+### SBATCH -p serial_requeue       # Partition to submit to
+### SBATCH --mem=100               # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o hydra_%j.out      # File to which STDOUT will be written
 #SBATCH -e hydra_%j.err      # File to which STDERR will be written
 #SBATCH --mail-type=END         # Type of email notification- BEGIN,END,FAIL,ALL
