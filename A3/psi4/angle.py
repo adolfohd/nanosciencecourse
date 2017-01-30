56aecffb0a4dbb5c05cba6efa4afdd1b625e1879
Christian Ruiz <crshadow18@gmail.com>
2017-01-30 18:34:16 -0500

    CO2 FTW

---
+++ A3/psi4/angle.py
@@ -0,0 +1,10 @@
+import math
+
+def dotproduct(v1, v2):
+  return sum((a*b) for a, b in zip(v1, v2))
+
+def length(v):
+  return math.sqrt(dotproduct(v, v))
+
+def angle(v1, v2):
+  return math.acos(dotproduct(v1, v2) / (length(v1) * length(v2)))
